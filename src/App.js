2f9687e6feb36c25c8215443da2b198e4a10d2be
limch <limch3125@gmail.com>
2022-10-05 22:53:45 +0900

    Done getting datas of coins

--- src/App.js
+++ src/App.js
@@ -2,25 +2,34 @@ import Button from './Button';
 import styles from './App.module.css';
 import {useEffect, useState} from 'react';
 
-function Hello() {
-  function by() { console.log("Bye"); }
-  function hi() {console.log("Hi"); return by; }
-
-  useEffect(hi, []);
-  return <div className='Hello'>Hello</div>;
-}
-
 function App() {
-  //console.log("Render App");
-  const [state, setState] = useState(false);
-  const onClick = () => {setState(!state); }
+	const [loading, setLoading] = useState(true);
+	const [coinList, setCoinList] = useState([]);
 
-  return (
-    <div className="App">
-      {state ? <Hello /> : null}
-      <button onClick={onClick}>{state ? "Hide" : "Show"}</button>
-    </div>
-  );
+	useEffect(() =>{
+		fetch('https://api.coinpaprika.com/v1/tickers')
+			.then((response) => response.json())
+			.then((json) => {
+				setLoading(false);
+				setCoinList(json)
+					console.log(json);
+			})
+	}, []);
+	
+		return (
+			<div className='App'>
+				<h1>The Coins!</h1>
+				<hr />
+				{loading ? <strong>Loading...</strong> : 
+				<select>
+					{coinList.map((coin) => (
+						<option>
+							{coin.name} - {coin.symbol} - ${coin.quotes.USD.price}
+						</option>
+					))}
+				</select>}
+				
+			</div>)
 }
 
 export default App;
