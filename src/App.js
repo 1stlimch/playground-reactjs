1f79195c73be5ff7cf3cc7f76fb957571820164d
limch <limch3125@gmail.com>
2022-10-05 19:57:18 +0900

    Done leaning theory

--- src/App.js
+++ src/App.js
@@ -2,24 +2,23 @@ import Button from './Button';
 import styles from './App.module.css';
 import {useEffect, useState} from 'react';
 
+function Hello() {
+  function by() { console.log("Bye"); }
+  function hi() {console.log("Hi"); return by; }
+
+  useEffect(hi, []);
+  return <div className='Hello'>Hello</div>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const countUp = () => setValue(counter + 1);
-
-  const [keyword, setKeyword] = useState('');
-  const onChange = (e) => setKeyword(e.target.value);
-
-  console.log('rendered');
-  useEffect(() => { console.log(`Call the api`) }, []);
-  useEffect(() =>{ 
-    if (keyword !== "" && keyword.length > 5) { console.log("Search for: ", keyword);}
-  }, [keyword]);
+  //console.log("Render App");
+  const [state, setState] = useState(false);
+  const onClick = () => {setState(!state); }
 
   return (
     <div className="App">
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here" />
-      <h1>{counter}</h1>
-      <button onClick={countUp}>Count Up</button>
+      {state ? <Hello /> : null}
+      <button onClick={onClick}>{state ? "Hide" : "Show"}</button>
     </div>
   );
 }
