1d49ca4c4fff39803da351ab845fc2160427516c
LCH <limch4531@office.kumoh.ac.kr>
2022-10-06 17:58:30 +0900

    middle of movie

--- src/App.js
+++ src/App.js
@@ -1,35 +1,22 @@
-import Button from './Button';
-import styles from './App.module.css';
-import {useEffect, useState} from 'react';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Link
+} from "react-router-dom";
+
+import Home from "./routes/Home";
+import Detail from "./routes/Detail"
 
 function App() {
-	const [loading, setLoading] = useState(true);
-	const [coinList, setCoinList] = useState([]);
-
-	useEffect(() =>{
-		fetch('https://api.coinpaprika.com/v1/tickers')
-			.then((response) => response.json())
-			.then((json) => {
-				setLoading(false);
-				setCoinList(json)
-					console.log(json);
-			})
-	}, []);
-	
-		return (
-			<div className='App'>
-				<h1>The Coins!</h1>
-				<hr />
-				{loading ? <strong>Loading...</strong> : 
-				<select>
-					{coinList.map((coin) => (
-						<option>
-							{coin.name} - {coin.symbol} - ${coin.quotes.USD.price}
-						</option>
-					))}
-				</select>}
-				
-			</div>)
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/detail" element={<Detail />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
